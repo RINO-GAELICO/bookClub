9636664c74651e324c753dd39dd78ebb5f92f5e7
RINO-GAELICO <ducciorocca@gmail.com>
2025-03-06 11:34:05 -0800

    Remove debugging checks for GCP credentials file in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,5 @@ echo "$GOOGLE_APPLICATION_CREDENTIALS_CONTENT" | base64 --decode > /tmp/gcp-key.
 # Set the environment variable to point to this file
 export GOOGLE_APPLICATION_CREDENTIALS="/tmp/gcp-key.json"
 
-# Debugging: Check if the file exists and print first 10 lines
-ls -lah /tmp/gcp-key.json
-head -n 10 /tmp/gcp-key.json
-
 # Start the app
 exec npm start
